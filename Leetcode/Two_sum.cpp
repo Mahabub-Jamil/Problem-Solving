aa7ae1963e0e57689d5f4cf09543d716a86ab957
MrError10 <rmahabub03@gmail.com>
2024-07-24 22:22:07 +0600

    Leetcode twoSum

---
+++ Leetcode/Two_sum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (nums[i] + nums[j] == target) {
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+            }
+        }
+        return result; 
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> indices = sol.twoSum(nums, target);
+    cout << "[" << indices[0] << ", " << indices[1] << "]" << endl;
+    return 0;
+}
