223e9069ea4891dd2990abf15951617d9d5c48e4
MrError10 <rmahabub03@gmail.com>
2024-08-01 11:12:05 +0600

    Codeforce 977A solved

---
+++ CodeForce/977A_Wrong_substraction.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < k; i++)
+    {
+        int lastDigit = n % 10;
+        if (lastDigit != 0)
+        {
+            n--;
+        }
+        else
+        {
+            n = n / 10;
+        }
+    }
+    cout << n;
+}
